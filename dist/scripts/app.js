39fa026cbaa3f3741c22ebd496f9be1315604dda
Tomiwa Adelae <thetommedia@gmail.com>
2024-01-15 17:50:56 +0100

    Made some changes

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -4,7 +4,9 @@ const hamburgerMenu = document.querySelector(".burger");
 
 // Events to trigger the nav bar being opened
 hamburgerMenu.addEventListener("click", (e) => {
+	// Make the Nav links display
 	nav.classList.toggle("links-open");
 
+	// Toggle the hamburger menu to a close icon
 	hamburgerMenu.classList.toggle("fa-times");
 });

ad3d431c1db4e019c1132dfb0de1dda2f8e5e019
Tomiwa Adelae <thetommedia@gmail.com>
2024-01-19 13:50:59 +0100

    Made the typewriter effect in the showcase section

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -10,3 +10,77 @@ hamburgerMenu.addEventListener("click", (e) => {
 	// Toggle the hamburger menu to a close icon
 	hamburgerMenu.classList.toggle("fa-times");
 });
+
+// Typewriter effect on the Home page showcase
+class Typewriter {
+	constructor(txtElement, words, wait = 3000) {
+		console.log("yes");
+		this.txtElement = txtElement;
+		this.words = words;
+		this.txt = "";
+		this.wordIndex = 0;
+		this.wait = parseInt(wait, 10);
+		this.type();
+		this.isDeleting = false;
+	}
+
+	type() {
+		// Current Index of words
+		const current = this.wordIndex % this.words.length;
+
+		// Get full text of the word
+		const fullTxt = this.words[current];
+
+		// Check if delecting
+		if (this.isDeleting) {
+			// Remove a character
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			// Add a character
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+		// Insert txt into Element
+		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+		// Initial Type speed
+		let typeSpeed = 300;
+
+		if (this.isDeleting) {
+			typeSpeed /= 2;
+		}
+
+		// Check and see if the word is complete
+		if (!this.isDeleting && this.txt === fullTxt) {
+			// Make a pause at the end
+			typeSpeed = this.wait;
+
+			// Set delete to true
+			this.isDeleting = true;
+		} else if ((this, this.isDeleting && this.txt === "")) {
+			this.isDeleting = false;
+
+			// Move to the next word
+
+			this.wordIndex++;
+
+			// Pause before start typing
+			typeSpeed = 500;
+		}
+
+		setTimeout(() => this.type(), typeSpeed);
+	}
+}
+
+// Init Typewriter effect on the DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init Typewriter effect
+function init() {
+	const txtElement = document.querySelector(".txt-type");
+	const words = JSON.parse(txtElement.getAttribute("data-words"));
+	const wait = txtElement.getAttribute("data-wait");
+
+	// Init Typewriter
+	new Typewriter(txtElement, words, wait);
+}
